992d9ab861bd2978e4d7e72e43d0cef1e3c3b19b
David Noguera Gutierrez <nogueradavid@hotmail.com>
2014-12-31 14:09:37 +0100

    Update Vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -2,14 +2,9 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "CentOs6.4_x86_64"
-  config.vm.box_url = "https://github.com/2creatives/vagrant-centos/releases/download/v6.4.2/centos64-x86_64-20140116.box"
-
+  config.vm.box = "precise32"
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
   config.vm.provision :shell, path: "bootstrap.sh"
-  config.vm.network :forwarded_port, guest: 80, host: 8080
-  config.vm.network :forwarded_port, guest: 3306, host: 3307
+  config.vm.network :forwarded_port, host: 8081, guest: 80
+  config.vm.network :forwarded_port, guest: 3306, host: 3308
 end
-
-
-# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
-VAGRANTFILE_API_VERSION = "2"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
 apt-get update
+
+#install Apache2
 apt-get install -y apache2
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant/httpdocs /var/www
 fi
 
-#instalación MYSQL-SERVER
+#install MYSQLSERVER
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password Admin123'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Admin123'
 apt-get install -y mysql-server
 if [ ! -f /var/log/databasesetup ];
 then
-    echo "CREATE USER 'comunidad'localhost'%' IDENTIFIED BY 'comunidad'" | mysql -uroot -pAdmin123
+    echo "CREATE USER 'comunidad'@'%' IDENTIFIED BY 'comunidad'" | mysql -uroot -pAdmin123
     echo "CREATE USER 'comunidad'@'localhost' IDENTIFIED BY 'comunidad'" | mysql -uroot -pAdmin123
     echo "CREATE DATABASE comunidad" | mysql -uroot -pAdmin123
-    echo "GRANT ALL ON comunidad.* TO 'comunidad'@'localhost'" | mysql -uroot -pAdmin123
+    echo "GRANT ALL ON comunidad.* TO 'comunidad'@'%'" | mysql -uroot -pAdmin123
     echo "GRANT ALL ON comunidad.* TO 'comunidad'@'localhost'" | mysql -uroot -pAdmin123
     echo "flush privileges" | mysql -uroot -pAdmin123
 
@@ -29,13 +31,13 @@ then
 fi
 
 
-# instalación PHP5
+# install PHP5
 sudo apt-get install php5 libapache2-mod-php5 php5-mysql -y
  
-# Reiniciamos el servidor web:
+# Restart Apache2
 sudo /etc/init.d/apache2 restart
-echo "ServerName localhost" | sudo tee /etc/apache2/sites-available/fqdn.conf
+echo "ServerName comunidad.local.com" | sudo tee /etc/apache2/sites-available/fqdn.conf
 sudo ln -s /etc/apache2/sites-available/fqdn.conf /etc/apache2/sites-enabled/fqdn.conf
  
-# Reiniciamos nuevamente el servidor web:
+# Restart Apache2 again
 sudo /etc/init.d/apache2 restart
